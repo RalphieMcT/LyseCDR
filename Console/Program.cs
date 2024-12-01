f4a03627c557a4d844bb256150ed781e687f1743
RalphieMcT <rolf.hjdal@gmail.com>
2024-12-01 19:05:25 +0100

    La til feilhåndtering i main-loop

--- Console/Program.cs
+++ Console/Program.cs
@@ -1,4 +1,7 @@
-﻿using CallDataRecord.Models;
+﻿using System.Data;
+using System.Security;
+using CallDataRecord.Models;
+using Newtonsoft.Json;
 
 namespace CallDataRecord;
 
@@ -10,26 +13,52 @@ public class Program
         {
             throw new ArgumentException("Filnavn mangler");
         }
-        
-        string filename = args[0];
-        var data = DataLoader.ReadCallDataRecordFile(filename);
-        LinqAnalyzer analyzer = new LinqAnalyzer(data);
-        
-        //Print most active Callers
-        var callers = analyzer.ActiveCallers();
-        var mostActiveCallers = callers.Take(3);
-        Console.WriteLine("Top 3 Most Active Callers:");
-        foreach (var caller in mostActiveCallers)
-        {
-            Console.WriteLine($"{caller.Key}: {caller.Value} calls");
-        }
-        
-        //Print Total duration of the most active caller
-        TotalDuration totalDuration = analyzer.MostActiveCallersTotalDuration();
-        Console.WriteLine($"Total Duration of Calls to {totalDuration.Number}: {totalDuration.Seconds} seconds");
 
-        //Print total unique phone numbers in dataset
-        int uniqueCount = analyzer.UniquePhoneNumbers();
-        Console.WriteLine($"Total Unique Phone Numbers: {uniqueCount}");
+        try
+        {
+            string filename = args[0];
+            var json = ReadFile(filename);
+            List<CDR> data = DeserializeJson(json);
+            LinqAnalyzer analyzer = new LinqAnalyzer(data);
+
+            //Print most active Callers
+            var callers = analyzer.ActiveCallers();
+            var mostActiveCallers = callers.Take(3);
+            Console.WriteLine("Top 3 Most Active Callers:");
+            foreach (var caller in mostActiveCallers)
+            {
+                Console.WriteLine($"{caller.Key}: {caller.Value} calls");
+            }
+
+            //Print Total duration of the most active caller
+            TotalDuration totalDuration = analyzer.MostActiveCallersTotalDuration();
+            Console.WriteLine($"Total Duration of Calls to {totalDuration.Number}: {totalDuration.Seconds} seconds");
+
+            //Print total unique phone numbers in dataset
+            int uniqueCount = analyzer.UniquePhoneNumbers();
+            Console.WriteLine($"Total Unique Phone Numbers: {uniqueCount}");
+
+            Console.ReadKey();
+        }
+        catch (Exception e)
+        {
+            Console.WriteLine(e);
+            Console.WriteLine("Press any key to exit");
+            Console.ReadKey();
+            Environment.Exit(1);
+        }
+    }
+
+    private static List<CDR> DeserializeJson(string json)
+    {
+        List<CDR> data = JsonConvert.DeserializeObject<List<CDR>>(json);
+        return data;
+    }
+
+    private static string ReadFile(string filename)
+    {
+        string path = Path.Join(Environment.CurrentDirectory, filename);
+        string json = File.ReadAllText(path);
+        return json;
     }
 }
